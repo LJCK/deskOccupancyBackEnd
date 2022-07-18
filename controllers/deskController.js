0bf6a76b4befa3b67142ca7a1664966f61ee23c8
LJCK <50812564+LJCK@users.noreply.github.com>
2022-07-18 08:48:35 +0800

    update

--- controllers/deskController.js
+++ controllers/deskController.js
@@ -4,18 +4,21 @@ const moment = require("moment");
 const get_desk_status=async(req,res)=>{
   const query_level = req.query.level
   const floor = await newOccupancy.findOne({level:query_level})
-  occupancy_status={}
+  occupancy_array=[]
   for(let desk_id in floor.desks){
     let expiry_time = moment(floor.desks[desk_id]["expiryTime"],"hh:mm:ss");
     console.log(expiry_time)
+    occupancy_status={}
     if(floor.desks[desk_id]["expiryTime"]===null || expiry_time.isBefore(moment.utc().local())){
       occupancy_status[desk_id]='unoccupied'
+      
     }else{
       occupancy_status[desk_id]='occupied'
     }
+    occupancy_array.push(occupancy_status)
   }
   
-  res.send(occupancy_status)
+  res.send(occupancy_array)
 }
 
 const get_all_levels=async(req,res)=>{
