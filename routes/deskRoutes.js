e7ea3776e3e81d643b9fd5872f46156946224dbe
LJCK <50812564+LJCK@users.noreply.github.com>
2022-07-20 15:23:56 +0800

    MVP

--- routes/deskRoutes.js
+++ routes/deskRoutes.js
@@ -2,9 +2,6 @@ const express = require('express');
 const deskController = require('../controllers/deskController')
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-  res.send("Welcome to backend");
-})
 router.get('/getDeskStatus', deskController.get_desk_status)
 router.get('/getAllLevels',deskController.get_all_levels)
 
