d81e70f5bbf8fedfc4c420c308bc2c0b42cc4715
LJCK <50812564+LJCK@users.noreply.github.com>
2022-07-26 18:03:46 +0800

    Update floorPlanController.js

--- controllers/floorPlanController.js
+++ controllers/floorPlanController.js
@@ -1,37 +1,47 @@
 const imageModel = require('../models/floorPlan')
 const fs = require("fs");
 
-const uploadFloorPlan=(req, res) => {
-  const saveImage =  imageModel({
-    _id: req.body.id,
+const uploadFloorPlan=async(req, res) => {
+  const existingFloorPlan = await imageModel.findOneAndReplace({_id: req.body.UID},{
     img: {
-      data: fs.readFileSync("uploads/" + req.file.filename),
-      // data:req.file.filename,
-      contentType: "image/png",
-    },
-  });
-  saveImage
-    .save()
-    .then((res) => {
-      console.log("image is saved");
-    })
-    .catch((err) => {
-      console.log(err, "error has occur");
+    data: fs.readFileSync("uploads/" + req.file.filename),
+    // data:req.file.filename,
+    contentType: "image/png",
+  }})
+  if(existingFloorPlan){
+    res.status(200).send('Floor plan is updated')
+  }else{
+    const saveImage =  imageModel({
+      _id: req.body.id,
+      img: {
+        data: fs.readFileSync("uploads/" + req.file.filename),
+        // data:req.file.filename,
+        contentType: "image/png",
+      },
     });
-    res.send('image is saved')
+    saveImage
+      .save()
+      .then((res) => {
+        res.status(200).send('Floor plan is uploaded')
+      })
+      .catch((err) => {
+        res.send(err)
+      });
+  }
+  
 }
 
 const getFloorPlan=async(req,res)=>{
   try{
     const data = []
-    const image =await imageModel.findById({_id:req.query.filename}).catch(err =>console.log(err))
+    const image =await imageModel.findById({_id:req.query.UID}).catch(err =>console.log(err))
     if(image === null){
       throw new Error("Image is null")
     }
     data.push(image)
     res.json(data)
   }catch(err){
-    console.log(err)
+    res.send(err)
   }
   
 }
