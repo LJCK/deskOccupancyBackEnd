ae04238da3e5e305356cc8eb7510c2e87ea57c4b
qinxxxm <soldiero3of@gmail.com>
2022-09-20 16:34:40 +0800

    ok

--- routes/sensorRoutes.js
+++ routes/sensorRoutes.js
@@ -5,11 +5,11 @@ const requireAuth = require('../middleware/requireAuth')
 
 router.get('/getSensorStatus', sensorController.get_sensor_status) // for sensor display page
 router.get('/getAllLevels',sensorController.get_all_levels) // for nav bar
+router.post('/addSensor',sensorController.add_sensor) 
 
 router.use(requireAuth)
-router.get('/getAllSensors', sensorController.get_all_sensors) // for displaying all sensors in a table //ok
-router.post('/addSensor',sensorController.add_sensor) //ok
-router.delete('/deleteSensor',sensorController.delete_sensor) //ok
+router.get('/getAllSensors', sensorController.get_all_sensors) // for displaying all sensors in a table
+router.delete('/deleteSensor',sensorController.delete_sensor) 
 // router.post('/permitJoin', sensorController.permit_join)
 // router.post('/pairDevice', sensorController.pairDevice)
 
