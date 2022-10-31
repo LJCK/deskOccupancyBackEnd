e80c45c6c8d5e0123dd002d820101a1dbc78ebde
Liang Zhiheng <liangzhiheng62@gmail.com>
2022-10-31 11:57:07 +0800

    update

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const sensorRoutes = require('./routes/sensorRoutes')
-const floorPlanRoutes = require('./routes/floorPlanRoutes')
-const sensorController = require('./controllers/sensorController')
-const userRoutes = require('./routes/userRoutes')
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 const cron = require('node-cron');
 const dotenv = require("dotenv")
 dotenv.config()
@@ -23,9 +23,36 @@ var corsOptions = {
 // const dbURI = 'mongodb+srv://zhiheng:zhiheng@cluster0.s7nla.mongodb.net/?retryWrites=true&w=majority'
 const dbURI = process.env.MONGODBURL
 // const dbURI = 'mongodb+srv://qinxiang:qinxiang@cluster0.ojjsesl.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true }) //its an async task, returns something like a promise
-        .then(result => app.listen(PORT, () => console.log('connected to db, server started in port ', PORT)))
-        .catch(err => console.log(err));
+// mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true }) //its an async task, returns something like a promise
+//         .then(result => app.listen(PORT, () => console.log('connected to db, server started in port ', PORT)))
+//         .catch(err => console.log(err));
+
+const conn = mongoose.createConnection(dbURI)
+let gfs;
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);  
+  gfs.collection('uploads');
+  app.listen(PORT, () => console.log('connected to db, server started in port ', PORT));
+});
+
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: dbURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+        const filename = file.originalname;
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+    });
+  }
+});
+
+const upload = multer({ storage });
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -35,11 +62,17 @@ app.use(bodyParser.urlencoded({ extended: true })) // for parsing application
 app.use(cors(corsOptions))
 
 app.get('/', (req,res)=>{
-    res.send("Welcome to backend");
+  gfs.files.find().toArray((err, files) => {
+    return res.json(files);
   })
-app.use('/sensor',sensorRoutes)
-app.use('/floorPlan',floorPlanRoutes)
-app.use('/user', userRoutes)
+})
+
+app.post('/upload',upload.single("file"),(req,res)=>{
+  res.json({ file: req.file });
+})
+
+
+
 // cron job reference 
 // * * * * * *
 // second (optional), minute, hour, day of month, month, day of week( 0 - 7, where 0 or 7 are sunday)
